9825b0c57d1992a1bba3b83838be989fd1c2bc1e
ZHUOMA <markus.zhuo@oocl.com>
2019-07-19 18:51:17 +0800

    project finished

---
+++ learn-tdd-in-vue/src/App.vue
@@ -0,0 +1,26 @@
+ <template>
+   <div>
+    <new-message-form @send="addMessage" />
+   </div>
+ </template>
+
+ <script>
+import NewMessageForm from './components/NewMessageForm';
+import MessageList from './components/MessageList';
+ export default {
+   name: 'App',
+   components: {
+    NewMessageForm,
+  },
+  data() {
+    return {
+      messages: [],
+    };
+  },
+  methods: {
+    addMessage(text) {
+      this.messages.unshift(text);
+    },
+  },
+ };
+ </script>
